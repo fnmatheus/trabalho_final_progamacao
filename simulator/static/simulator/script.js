8650b2fdd398e3566af6f788311ed69826dc6338
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2025-06-28 10:33:31 -0300

    feat: adding amount settings in devices table

--- simulator/static/simulator/script.js
+++ simulator/static/simulator/script.js
@@ -1,3 +1,29 @@
+// função responsável por iniciar a tabela de dispositivos ja pre definida
+function deviceAmountSettings(device) {
+    const tdAmount = document.createElement("td");
+
+    const amountValue = document.createElement("span");
+    amountValue.textContent = device.amount;
+    
+    const btnMinus = document.createElement("button");
+    btnMinus.textContent = "-";
+    btnMinus.onclick = () => {
+        if (amountValue.textContent > 0) {
+            amountValue.textContent = parseInt(amountValue.textContent) - 1;
+        }
+    }
+
+    const btnPlus = document.createElement("button");
+    btnPlus.textContent = "+";
+    btnPlus.onclick = () => {
+        amountValue.textContent = parseInt(amountValue.textContent) + 1;
+    }
+
+    tdAmount.append(btnMinus, amountValue, btnPlus);
+
+    return tdAmount;
+}
+
 function initDevicesTable() {
     const devices = [
         { name: "Device 1", amount: 2, consumption: 150 },
@@ -10,11 +36,10 @@ function initDevicesTable() {
         const line = document.createElement("tr");
 
         const tdName = document.createElement("td");
-        const tdAmount = document.createElement("td");
         const tdConsumption = document.createElement("td");
 
         tdName.textContent = device.name;
-        tdAmount.textContent = device.amount;
+        tdAmount = deviceAmountSettings(device);
         tdConsumption.textContent = device.consumption;
 
         line.append(tdName, tdAmount, tdConsumption);

e26b899bdec7c7eac5e919e0172eafdd32a142ab
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2025-06-28 15:44:41 -0300

    feat: add new device in table

--- simulator/static/simulator/script.js
+++ simulator/static/simulator/script.js
@@ -1,20 +1,46 @@
-// função responsável por iniciar a tabela de dispositivos ja pre definida
+// função que cria cada celula da tabela e criar os elementos dela
 function deviceInfoCell(device, type) {
     const tdCell = document.createElement("td");
-
+    
     const cellValue = document.createElement("input");
     cellValue.value = device[type];
-
+    
     if (type != "name") {
         cellValue.type = "number"
         cellValue.min = "0"
     }
-
+    
     tdCell.append(cellValue);
-
+    
     return tdCell;
 }
 
+// Código responsável por criar uma linha da tabela
+function deviceLineCreator(device) {
+    const line = document.createElement("tr");
+        
+    const tdName = deviceInfoCell(device, "name");
+    const tdAmount = deviceInfoCell(device, "amount");
+    const tdAveragePower = deviceInfoCell(device, "averagePower");
+    const tdUsageTime = deviceInfoCell(device, "usageTime");
+
+    const tdDelete = document.createElement("td");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "🗑️"
+    deleteBtn.onclick = () => {
+        const auth = confirm("Tem certeza que deseja remover este dispositivo?");
+        if (auth) {
+            line.remove();
+        }
+    };
+    tdDelete.appendChild(deleteBtn);
+
+    line.append(tdName, tdAmount, tdAveragePower, tdUsageTime, tdDelete);
+    
+    return line;
+}
+
+// função responsável por iniciar a tabela de dispositivos ja pre definida
 function initDevicesTable() {
     const devices = [
         { name: "Computador", amount: 40, averagePower: 145, usageTime: 9 },
@@ -26,34 +52,41 @@ function initDevicesTable() {
 
     const tableBody = document.querySelector("#devices-table tbody");
 
-    devices.forEach((device, i) => {
-        const line = document.createElement("tr");
-        
-        const tdName = deviceInfoCell(device, "name");
-        const tdAmount = deviceInfoCell(device, "amount");
-        const tdAveragePower = deviceInfoCell(device, "averagePower");
-        const tdUsageTime = deviceInfoCell(device, "usageTime");
-
-        const tdDelete = document.createElement("td");
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "🗑️"
-        deleteBtn.onclick = () => {
-            const auth = confirm("Tem certeza que deseja remover este dispositivo?");
-            if (auth) {
-                line.remove();
-            }
-        };
-        tdDelete.appendChild(deleteBtn);
-
-        line.append(tdName, tdAmount, tdAveragePower, tdUsageTime, tdDelete);
+    devices.forEach((device) => {
+        const line = deviceLineCreator(device);
 
         tableBody.appendChild(line);
     })
 }
 
+// função responsável por alterar a visibilidade do formulario de adicionar novo dispositivo
 function addDeviceFormDisplay(stats) {
-    addDeviceForm = document.getElementById("device-form");
-    addDeviceForm.style.display = stats;
+    const addDeviceFormDiv = document.getElementById("device-form");
+    addDeviceFormDiv.style.display = stats;
 }
+
+// trecho do código para adicionar novo dispositivo na tabela
+const addDeviceForm = document.querySelector("#device-form form");
+addDeviceForm.addEventListener("submit", (event) => {
+    // previne que ao submeter o formulario faça alguma coisa indesejada
+    event.preventDefault();
+
+    const tableBody = document.querySelector("#devices-table tbody");
+    
+    const data = new FormData(addDeviceForm);
+
+    const deviceInfo = {
+        name: data.get("name"),
+        amount: data.get("amount"),
+        averagePower: data.get("averagePower"),
+        usageTime: data.get("usageTime"),
+    };
+
+    const newLine = deviceLineCreator(deviceInfo);
+    tableBody.appendChild(newLine);
+
+    addDeviceFormDisplay('none');
+    alert("Novo dispositivo adicionado!")
+});
 
 initDevicesTable();

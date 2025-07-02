4ae9c48b6ed3c00a378f8d2289b75e2e5e24bf7f
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2025-07-01 23:55:55 -0300

    feat: finish plot generation

--- simulator/views.py
+++ simulator/views.py
@@ -56,20 +56,38 @@ def get_total_cost(devices):
     return total_cost
 
 
+# Caminho no qual os graficos devem ser salvos
 path = "simulator/static/simulator/plots/"
 
 
-def plot_simple_bar(devices, name, title, xlabel, ylabel):
-    labels = [device["name"] for device in devices]
-    costs = [device["cost"] for device in devices]
+# Função para gerar o gráfico de visão geral dos gastos
+def plot_daily_cost_value(daily_cost, total_daily_cost, days, name):
+    days_range = list(range(1, days + 1))
+
+    # cria uma lista com o nome e outra lista de listas com cada gasto diario
+    # de forma acumulativa soma o dia anterior
+    names = [device["name"] for device in daily_cost]
+    costs = [device["cost"] for device in daily_cost]
+    cumulative_daily_cost = [
+        [round(cost * day, 2) for day in days_range] for cost in costs
+    ]
+
+    # cria uma lista que forma uma lista acumula o valor do dia anterior
+    cumulative_total_cost = [
+        round(total_daily_cost * day, 2) for day in days_range
+    ]
 
     plt.figure(figsize=(16, 9))
 
-    plt.bar(labels, costs, color="#ff9900")
+    for i, cost in enumerate(cumulative_daily_cost):
+        plt.plot(days_range, cost, label=names[i])
 
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
+    plt.plot(days_range, cumulative_total_cost, label="Total")
+
+    plt.xlabel("Período em dias")
+    plt.ylabel("Gasto Enegético em R$")
+    plt.title("Custo diário")
+    plt.legend()
 
     plt.savefig(f"{path}{name}.svg", dpi=300, bbox_inches='tight')
     plt.close()
@@ -77,12 +95,32 @@ def plot_simple_bar(devices, name, title, xlabel, ylabel):
     return {"path": f"{path}{name}.svg"}
 
 
-def plot_double_bar(units, groups, name, title, xlabel, ylabel):
-    labels = [device["name"] for device in units]
+# Função responsável em gerar o gráfico de barra única
+def plot_simple_bar(devices, name, days):
+    names = [device["name"] for device in devices]
+    costs = [device["cost"] for device in devices]
+
+    plt.figure(figsize=(16, 9))
+
+    plt.bar(names, costs, color="#ff9900")
+
+    plt.xlabel("Dispositivos")
+    plt.ylabel("Gasto Enegético em R$")
+    plt.title(f"Consumo em {days} dias por dispositivo")
+
+    plt.savefig(f"{path}{name}.svg", dpi=300, bbox_inches='tight')
+    plt.close()
+
+    return {"path": f"{path}{name}.svg"}
+
+
+# Função responsável em gerar o gráfico de barra duplas para comparação
+def plot_double_bar(units, groups, name):
+    names = [device["name"] for device in units]
     units_costs = [device["cost"] for device in units]
     groups_costs = [device["cost"] for device in groups]
 
-    x_position = np.arange(len(labels))
+    x_position = np.arange(len(names))
     bar_width = 0.35
     first_position = x_position - bar_width/2
     second_position = x_position + bar_width/2
@@ -104,10 +142,11 @@ def plot_double_bar(units, groups, name, title, xlabel, ylabel):
         color="#ff9900"
     )
 
-    plt.xticks(x_position, labels)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
+    plt.xticks(x_position, names)
+    plt.xlabel("Dispositivos")
+    plt.ylabel("Gasto Enegético em R$")
+    plt.title("Consumo de Unidade x Grupo de Dispositivos")
+    plt.legend()
 
     plt.savefig(f"{path}{name}.svg", dpi=300, bbox_inches='tight')
     plt.close()
@@ -115,6 +154,9 @@ def plot_double_bar(units, groups, name, title, xlabel, ylabel):
     return {"path": f"{path}{name}.svg"}
 
 
+# Função principal da simulação de gastos
+# Está função utiliza as outras funções e reuni todos os dados necessários
+# Dá o retorno para o frontend
 def simulate(request: HttpRequest):
     if request.method == "POST":
         body = json.loads(request.body)
@@ -128,26 +170,29 @@ def simulate(request: HttpRequest):
         periodic_cost = get_devices_periodic_cost(daily_cost, days)
         total_periodic_cost = get_total_cost(periodic_cost)
 
+        daily_cost_plot = plot_daily_cost_value(
+            daily_cost,
+            total_daily_cost,
+            days,
+            "daily_cost_plot"
+        )
+
         periodic_plot = plot_simple_bar(
             periodic_cost,
             "periodic_cost_plot",
-            f"Consumo em {days} dias por dispositivo",
-            "Dispositivos",
-            "Gasto Enegético em R$",
+            days,
         )
 
         unitxgroup_plot = plot_double_bar(
             unit_daily_cost,
             daily_cost,
             "unitxgroup_plot",
-            "Consumo de Unidade x Grupo de Dispositivos",
-            "Dispositivos",
-            "Gasto Enegético em R$",
         )
 
         data = {
             "daily_cost": total_daily_cost,
             "periodic_cost": total_periodic_cost,
+            "daily_cost_plot": daily_cost_plot["path"],
             "periodic_plot": periodic_plot["path"],
             "unitxgroup_plot": unitxgroup_plot["path"],
         }
